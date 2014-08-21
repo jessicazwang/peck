9b710ae6a59227fcc59bee22a3cb84e9356eb144
jzwang <jzwang@mit.edu>
2014-08-21 04:21:35 -0400

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pkt.wsgi --debug --log-level debug
--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from website.models import Brother
+import requests
 
 
 def index(request):
